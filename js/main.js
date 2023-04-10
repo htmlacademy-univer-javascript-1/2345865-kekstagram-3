5f68d4234eab5d6f0a045b608f198449963fb384
Nick <signaalwespbij@yandex.ru>
2023-04-10 13:01:24 +0300

    getPhotosArray() and getPhoto()

--- js/main.js
+++ js/main.js
@@ -7,3 +7,17 @@ function getRandomInt(min, max) {
 function isShorter(str, size) {
   return str.length <= size;
 }
+
+function getPhoto(id) {
+  return {
+    id: id,
+    url: `photos/${id}.jpg`,
+    desciption: 'just photo',
+    likes: getRandomInt(15, 200),
+    comments: getRandomInt(0, 200)
+  }
+}
+
+function getPhotosArray(size) {
+  return Array.from({length: size}, (_, i) => getPhoto(i + 1));
+}
