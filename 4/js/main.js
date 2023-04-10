160d4dfa65a89a363e8f01952bd25632c2435093
Keks <keks@htmlacademy.ru>
2023-04-10 13:22:46 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/main.js
@@ -0,0 +1,23 @@
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function isShorter(str, size) {
+  return str.length <= size;
+}
+
+function getPhoto(id) {
+  return {
+    id: id,
+    url: `photos/${id}.jpg`,
+    desciption: 'just photo',
+    likes: getRandomInt(15, 200),
+    comments: getRandomInt(0, 200)
+  };
+}
+
+function getPhotosArray(size) {
+  return Array.from({length: size}, (_, i) => getPhoto(i + 1));
+}
