00a30ad19fe966fd8258841e7d8414ea55490c2f
cladium <signaalwespbij@yandex.ru>
2023-05-11 20:20:39 +0300

    add checks in form

---
+++ js/form.js
@@ -0,0 +1,38 @@
+import {isEscKey} from './util.js';
+
+const uploadButton = document.querySelector('#upload-file');
+const cancelButton = document.querySelector('#upload-cancel');
+const hashtag = document.querySelector('.text__hashtags');
+const comment = document.querySelector('.text__description');
+
+function purge() {
+  uploadButton.value = '';
+  hashtag.value = '';
+  comment.value = '';
+}
+
+function formEscHandler(evt) {
+  if (isEscKey(evt)) {
+    evt.preventDefault();
+    closeUploadForm();
+  }
+}
+
+function openUploadForm() {
+  document.body.classList.add('modal-open');
+  document.querySelector('.img-upload__overlay').classList.remove('hidden');
+  document.addEventListener('keydown', formEscHandler);
+}
+
+function closeUploadForm() {
+  document.body.classList.remove('modal-open');
+  document.querySelector('.img-upload__overlay').classList.add('hidden');
+  document.removeEventListener('keydown', formEscHandler);
+  purge();
+}
+
+//added opening function on downloadButton
+uploadButton.addEventListener('change', openUploadForm);
+
+//added closing function on cancelButton
+cancelButton.addEventListener('click', closeUploadForm);
